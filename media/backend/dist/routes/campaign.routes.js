40f4ffcf4ed034e140d7f755f4932237edc27389
vivek-dubey-0305 <vive.dubey0305@gmail.com>
2026-08-12 21:55:02 +0530

    Fix build for Render and untrack dist

--- media/backend/dist/routes/campaign.routes.js
+++
@@ -1,8 +0,0 @@
-import { createCampaign, addMedia, startCampaign, approvePlatformPost, getCampaign } from '../controllers/campaign.controller';
-export async function campaignRoutes(app) {
-    app.post('/', createCampaign);
-    app.get('/:id', getCampaign);
-    app.post('/:id/media', addMedia);
-    app.post('/:id/start', startCampaign);
-    app.post('/posts/:postId/approve', approvePlatformPost);
-}
--- media/backend/dist/routes/oauth.routes.js
+++
@@ -1,7 +0,0 @@
-import { OAuthController } from '../controllers/oauth.controller';
-export async function oauthRoutes(fastify) {
-    fastify.get('/oauth/:platform/url', OAuthController.getAuthorizationUrl);
-    fastify.get('/oauth/:platform/callback', OAuthController.handleCallback);
-    fastify.get('/social-accounts', OAuthController.getConnectedAccounts);
-    fastify.delete('/social-accounts/:id', OAuthController.disconnect);
-}
